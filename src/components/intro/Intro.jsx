f956570e865cd96aae7208da8a6130843f879982
Godwin-Softwares <godwin.softwares@gmail.com>
2022-02-25 18:32:02 +0100

    Update of portfolio

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import "./intro.css"
-import me from "../../img/godwin.png"
+import me from "../../img/pic.png"
 
 const intro = () => {
   return (
@@ -20,9 +20,14 @@ const intro = () => {
               </div>
             </div>
             <div className="i-desc">
-              I am a professional Software Developer, also vastly inclined in Mobile App Development with an exceptional expertise in website development coupled with Unit testing skill in DevOp and a wao touch in Frontend User experience in Designing
+              I am a professional Software Developer, vastly inclined in Mobile App Development with an exceptional expertise in website development coupled with Unit testing, skilled in DevOp technology and Frontend UI/UX.
+            </div>
+            <div className='i-button'>
+              <button id="#c">Rule Your World</button>
             </div>
           </div>
+
+          
         </div>
         <div className="i-right">
           <div className="i-bg"></div>
